d21451d09ed6046f11157f69ec56cdba76a6eca5
Milky Raven <heartonllamas@gmail.com>
2022-11-01 15:40:21 +0100

    starting iteration 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,9 +1,22 @@
+const Celebrity = require("../models/Celebrity.model");
+
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
 // all your routes here
-router.get("/", (req, res, next) => {
-    res.render("celebrities");
+router.get("/celebrities/create", (req, res, next) => {
+    res.render("celebrities/new-celebrity");
   });
+
+router.post("/celebrities/create", async(req, res) => {
+  try {
+    const newCelebrity = await Celebrity.create(req.body)
+    console.log("celebrity created")
+    res.redirect("/celebrities")
+  } catch (err) {
+    console.log(err)
+    res.render("/celebrities/create")
+  }
+})
 
 module.exports = router;
