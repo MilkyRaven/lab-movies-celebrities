7c8052f7209fbfd988acea4c1a669b71d11af260
Milky Raven <heartonllamas@gmail.com>
2022-11-01 17:45:26 +0100

    working on iteration 9

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,9 +1,33 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+
+const Movie = require("../models/Movie.model")
 const router = require("express").Router();
 
 // all your routes here
-router.get("/", (req, res, next) => {
-    res.render("movies");
+router.get("/", async (req, res, next) => {
+  try {
+    const movies = await Movie.find({})
+    console.log(movies)
+    res.render("movies/movies", {movies})
+  }catch (err) {
+    console.log(err)
+  }
+    res.render("movies/movies");
   });
 
+  router.get("/create", (req, res, next) => {
+    res.render("movies/new-movie");
+  });
+
+router.post("/create", async(req, res) => {
+  const {title, genre, plot, cast} = req.body
+  try {
+   await Movie.create({title, genre, plot, cast})
+   console.log("Movie created!")
+    res.redirect("/movies")
+  } catch (err) {
+    console.log(err)
+    res.redirect("/movies/new-movie")
+  }
+})
 module.exports = router;
